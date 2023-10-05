45d660f2ab594b407fe49d92643d17dddfcad8f8
Julia Khabibova <julia.khabibova@mail.ru>
2023-10-05 15:12:49 +0500

    fixed mistake

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const httpConstants = require('http2').constants;
 
 router.use('/users', require('./users'));
 router.use('/cards', require('./cards'));
 
 router.use('*', (req, res) => {
-  res.status(404).send({ message: 'страница не найдена' });
+  res.status(httpConstants.HTTP_STATUS_NOT_FOUND).send({ message: 'страница не найдена' });
 });
 
 module.exports = router;
