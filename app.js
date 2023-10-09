eedcbf786923eb4f1f0c54bc0947616f89993325
Julia Khabibova <julia.khabibova@mail.ru>
2023-10-09 16:20:46 +0500

    error refactoring

--- app.js
+++ app.js
@@ -17,4 +17,14 @@ app.use((req, res, next) => {
 
 app.use('/', require('./routes/index'));
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500
+      ? 'На сервере произошла ошибка'
+      : message,
+  });
+  next();
+});
+
 app.listen(PORT);
